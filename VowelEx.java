a1e3c183ef7a5c4c29c7c09f9c39c97413c33af5
Shubham2034 <136883801+Shubham2034@users.noreply.github.com>
2024-03-12 12:56:10 +0530

    Add files via upload

---
+++ VowelEx.java
@@ -0,0 +1,23 @@
+package com.classtest;
+
+import java.io.*; 
+
+public class VowelEx { 
+  
+    
+    static void Vowel_Or_Consonant(char y) 
+    { 
+        if (y == 'a' || y == 'e' || y == 'i' || y == 'o'
+            || y == 'u') 
+            System.out.println("It is a Vowel."); 
+        else
+            System.out.println("It is a Consonant."); 
+    } 
+  
+ 
+    static public void main(String[] args) 
+    { 
+        Vowel_Or_Consonant('b'); 
+        Vowel_Or_Consonant('u'); 
+    } 
+}
